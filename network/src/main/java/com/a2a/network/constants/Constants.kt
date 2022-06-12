d50c898720ac3382e3b5e7e994319700a824fcf3
firassalaman <fsalman@access2arabia.com>
2022-06-12 23:10:40 +0300

    Fix BASE_ENDPOINT

--- network/src/main/java/com/a2a/network/constants/Constants.kt
+++ network/src/main/java/com/a2a/network/constants/Constants.kt
@@ -3,9 +3,9 @@ package com.a2a.network.constants
 
 class Constants {
     companion object {
-        const val BASE_ENDPOINT = "A2AProcess"
+       // const val BASE_ENDPOINT = "A2AProcess"
         // for Encryption
-//      const val BASE_ENDPOINT = "A2AProcessEncrypt"
+      const val BASE_ENDPOINT = "A2AProcessEncrypt"
     }
 
 }
